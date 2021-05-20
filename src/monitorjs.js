95c8000af5a5db127a3330ff62d2b286c19710fd
jameszws <jameszws@outlook.com>
2021-05-20 16:35:17 +0800

    修复无法控制不收集jsError、promiseError、resourceError、ajaxError、consoleError的问题，并显式控制consoleError和vueError

--- src/monitorjs.js
+++ src/monitorjs.js
@@ -1,4 +1,4 @@
-import { AjaxError,ConsoleError,JsError,PromiseError,ResourceError,VueError } from './error';
+import { AjaxError, ConsoleError, JsError, PromiseError, ResourceError, VueError } from './error';
 import { AjaxLibEnum } from "./base/baseConfig.js";
 import MonitorPerformance from './performance';
 import MonitorNetworkSpeed from './performance/networkSpeed';
@@ -6,46 +6,46 @@ import './utils/extends';
 
 class MonitorJS {
 
-    constructor(){
+    constructor() {
         this.jsError = true;
         this.promiseError = true;
         this.resourceError = true;
         this.ajaxError = true;
         this.consoleError = false; //console.error默认不处理
-        this.vueError =  false;
+        this.vueError = false;
     }
-    
+
     /**
      * 处理异常信息初始化
      * @param {*} options 
-     */ 
-    init(options){
+     */
+    init(options) {
         options = options || {};
-        this.jsError = options.jsError || this.jsError;
-        this.promiseError = options.promiseError || this.promiseError;
-        this.resourceError = options.resourceError || this.resourceError;
-        this.ajaxError = options.ajaxError || this.ajaxError;
-        this.consoleError = options.consoleError || this.consoleError;
-        this.vueError = options.vueError || this.vueError;
+        this.jsError = !(options.jsError === false);
+        this.promiseError = !(options.promiseError === false);
+        this.resourceError = !(options.resourceError === false);
+        this.ajaxError = !(options.ajaxError === false);
+        this.consoleError = options.consoleError === true;  //显式配置
+        this.vueError = options.vueError === true;  //显式配置
         let reportUrl = options.url;//上报错误地址
         let extendsInfo = options.extendsInfo || {};  //扩展信息（一般用于系统个性化分析）
         let param = { reportUrl, extendsInfo };
-        if(this.jsError){
+        if (this.jsError) {
             new JsError(param).handleError();
         }
-        if(this.promiseError){
+        if (this.promiseError) {
             new PromiseError(param).handleError();
         }
-        if(this.resourceError){
+        if (this.resourceError) {
             new ResourceError(param).handleError();
         }
-        if(this.ajaxError){
+        if (this.ajaxError) {
             new AjaxError(param).handleError(AjaxLibEnum.DEFAULT);
         }
-        if(this.consoleError){
+        if (this.consoleError) {
             new ConsoleError(param).handleError();
         }
-        if(this.vueError && options.vue){
+        if (this.vueError && options.vue) {
             new VueError(param).handleError(options.vue);
         }
     }
@@ -54,14 +54,14 @@ class MonitorJS {
      * 监听页面性能
      * @param {*} options {pageId：页面标示,url：上报地址}
      */
-    monitorPerformance(options){
+    monitorPerformance(options) {
         options = options || {};
         new MonitorNetworkSpeed(options).reportNetworkSpeed();
-        let recordFunc = ()=>{
+        let recordFunc = () => {
             new MonitorPerformance(options).record();
         };
-        window.removeEventListener("unload",recordFunc);
-        window.addEventListener("unload",recordFunc);
+        window.removeEventListener("unload", recordFunc);
+        window.addEventListener("unload", recordFunc);
     }
 
 }
